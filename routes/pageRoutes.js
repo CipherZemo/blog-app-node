2ddfc0f6860006fd95f972638aa2ac24c0c47a9d
CipherZemo <acharyakrishnan9@gmail.com>
2025-12-23 14:51:28 +0530

    updated '/' route for pagination

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -1,8 +1,39 @@
 const express = require("express");
+const axios = require("axios");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.render("home", { title: "Home" });
+const API_BASE = "http://localhost:3000/api";
+
+router.get("/", async (req, res) => {
+  try {
+    const page = req.query.page || 1;
+    const search = req.query.search || "";
+
+    const response = await axios.get(`${API_BASE}/posts`, {
+      params: {
+        page,
+        limit: 5,
+        search
+      }
+    });
+
+    res.render("home", {
+      title: "Home",
+      posts: response.data.posts,
+      currentPage: response.data.page,
+      totalPages: response.data.totalPages,
+      search
+    });
+
+  } catch (err) {
+    res.render("home", {
+      title: "Home",
+      posts: [],
+      currentPage: 1,
+      totalPages: 1,
+      search: ""
+    });
+  }
 });
 
 router.get("/login", (req, res) => {
@@ -14,3 +45,4 @@ router.get("/signup", (req, res) => {
 });
 
 module.exports = router;
+
