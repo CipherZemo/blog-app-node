3b5c464c9b0f6e8b6eb3dbb8edbfaab249b8546f
CipherZemo <acharyakrishnan9@gmail.com>
2026-01-09 12:49:38 +0530

    role check backend

--- middleware/roleMiddleware.js
+++ middleware/roleMiddleware.js
@@ -6,9 +6,4 @@ exports.authorizeRoles = (...roles) => {//'...' are REST parameter syntax (to ac
     next();
   };
 };
-//here authorizeRoles is not exported as function. each exporting file is wrapped by node as follows
-// (function (exports, require, module, __filename, __dirname) {
 
-//    });
-//module.exports is the actual object your file returns , exports is simply a shortcut pointing to the same object(exports → module.exports → {}). ie, exports.authorizeRoles() exports it as "{ validatePost: [Function] }" which is an object but not a functn. so when you'll try to use it it wont be executed. so in routes  call it as const {valid}=require(....);
-//by using {} nodejs will deconsruct it and pullout the function residing in it, so functn can be executed.
