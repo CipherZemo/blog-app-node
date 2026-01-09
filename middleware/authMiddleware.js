ba9988351707908d4fa20ae770a7338fd633994a
CipherZemo <acharyakrishnan9@gmail.com>
2026-01-09 12:48:23 +0530

    auth backend

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {//A middleware must be a function with three arguments: req, res, next
 
-  const authHeader = req.headers.authorization;// Get token from header. When the frontend sends a protected request, it includes a header like Authorization: Bearer YOUR_TOKEN_HERE
+  const authHeader = req.headers.authorization;
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "No token provided" });
@@ -12,11 +12,8 @@ module.exports = (req, res, next) => {//A middleware must be a function with thr
   try {
     // Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    // Once the token is verified, JWT returns a decoded object.Attaching it to user request
-    req.user = decoded;
-
-    next();// next middleware or controller will run(routing chains)
+    req.user = decoded; // Once the token is verified, JWT returns the decoded object.Attaching it to user request
+    next();
 
   } catch (err) {
     res.status(401).json({ message: "Invalid token" });
